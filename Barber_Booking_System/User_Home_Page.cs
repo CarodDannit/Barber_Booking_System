b49ca7f8fd15195f2db3c6de958c94c7aa0fc36a
Carod <imran.danial2006@gmail.com>
2026-05-15 22:46:31 +0800

    ade database

--- Barber_Booking_System/User_Home_Page.cs
+++ Barber_Booking_System/User_Home_Page.cs
@@ -19,7 +19,37 @@ namespace Barber_Booking_System
 
         private void Form2_Load(object sender, EventArgs e)
         {
+            // TODO: This line of code loads data into the 'bekasIceCream.BOOKING' table. You can move, or remove it, as needed.
+            this.bOOKINGTableAdapter.Fill(this.bekasIceCream.BOOKING);
+            // TODO: This line of code loads data into the 'bekasIceCream.BARBER' table. You can move, or remove it, as needed.
+            this.bARBERTableAdapter.Fill(this.bekasIceCream.BARBER);
+            // TODO: This line of code loads data into the 'bekasIceCream.BARBER' table. You can move, or remove it, as needed.
+            this.bARBERTableAdapter.Fill(this.bekasIceCream.BARBER);
             Console.WriteLine("load hometyrtyrtyrtyrt page");
+        }
+
+        private void bARBERBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.bARBERBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.bekasIceCream);
+
+        }
+
+        private void bARBERBindingNavigatorSaveItem_Click_1(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.bARBERBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.bekasIceCream);
+
+        }
+
+        private void bOOKINGBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.bOOKINGBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.bekasIceCream);
+
         }
     }
 }
