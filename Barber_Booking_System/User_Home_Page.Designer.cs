34b32646b75ce08e5cfcf0c2c03d945d611e2054
Chernobylian-Crow <madlycrow@gmail.com>
2026-05-19 12:19:52 +0800

    testing changing datagridview

--- Barber_Booking_System/User_Home_Page.Designer.cs
+++ Barber_Booking_System/User_Home_Page.Designer.cs
@@ -30,38 +30,55 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
+            this.dgBooking = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgBooking)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Location = new System.Drawing.Point(16, 14);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(186, 46);
+            this.label1.Size = new System.Drawing.Size(257, 64);
             this.label1.TabIndex = 0;
             this.label1.Text = "Bookings";
             // 
             // button2
             // 
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(743, 440);
+            this.button2.Location = new System.Drawing.Point(1022, 660);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(187, 41);
+            this.button2.Size = new System.Drawing.Size(257, 62);
             this.button2.TabIndex = 10;
             this.button2.Text = "new booking";
             this.button2.UseVisualStyleBackColor = true;
             // 
+            // dgBooking
+            // 
+            this.dgBooking.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgBooking.Location = new System.Drawing.Point(85, 145);
+            this.dgBooking.Name = "dgBooking";
+            this.dgBooking.RowHeadersWidth = 72;
+            this.dgBooking.RowTemplate.Height = 31;
+            this.dgBooking.Size = new System.Drawing.Size(930, 424);
+            this.dgBooking.TabIndex = 11;
+            // 
             // User_Home_Page
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(942, 493);
+            this.ClientSize = new System.Drawing.Size(1295, 1029);
+            this.Controls.Add(this.dgBooking);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "User_Home_Page";
             this.Text = "Form2";
             this.Load += new System.EventHandler(this.Form2_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgBooking)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -71,5 +88,6 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridView dgBooking;
     }
 }
