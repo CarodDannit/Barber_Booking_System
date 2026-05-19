e312ff6694120f98628566936103c4077747c9e6
Hazeeq <raedhazeeq@gmail.com>
2026-05-19 12:22:09 +0800

    apapappapap

--- Barber_Booking_System/User_Home_Page.Designer.cs
+++ Barber_Booking_System/User_Home_Page.Designer.cs
@@ -38,20 +38,18 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(16, 14);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(12, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(257, 64);
+            this.label1.Size = new System.Drawing.Size(186, 46);
             this.label1.TabIndex = 0;
             this.label1.Text = "Bookings";
             // 
             // button2
             // 
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(1022, 660);
-            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button2.Location = new System.Drawing.Point(743, 440);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(257, 62);
+            this.button2.Size = new System.Drawing.Size(187, 41);
             this.button2.TabIndex = 10;
             this.button2.Text = "new booking";
             this.button2.UseVisualStyleBackColor = true;
@@ -59,22 +57,22 @@
             // dgBooking
             // 
             this.dgBooking.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgBooking.Location = new System.Drawing.Point(85, 145);
+            this.dgBooking.Location = new System.Drawing.Point(49, 308);
+            this.dgBooking.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.dgBooking.Name = "dgBooking";
             this.dgBooking.RowHeadersWidth = 72;
             this.dgBooking.RowTemplate.Height = 31;
-            this.dgBooking.Size = new System.Drawing.Size(930, 424);
+            this.dgBooking.Size = new System.Drawing.Size(676, 283);
             this.dgBooking.TabIndex = 11;
             // 
             // User_Home_Page
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1295, 1029);
+            this.ClientSize = new System.Drawing.Size(942, 686);
             this.Controls.Add(this.dgBooking);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.label1);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "User_Home_Page";
             this.Text = "Form2";
             this.Load += new System.EventHandler(this.Form2_Load);
